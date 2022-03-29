a858c600ac4ed415444f21b26afa8bdf8a486c40
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-29 16:11:34 +0530

    Method program

---
+++ src/src/Main.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Main {
+
+    public static void add()
+    {
+        int a=20;
+        int b=10;
+        System.out.println("sum= "+(a+b));
+    }
+    public static void main(String[]args){
+        Main obj=new Main();
+        obj.Disp();
+        Main.add();
+    }
+    void Disp()
+    {
+        System.out.println("Vasundhara");
+
+
+    }
+}
